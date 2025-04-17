28bc19f0f08a2c24d74619f5397412e20f86a1af
aelsi2 <eliseevandrey2005@gmail.com>
2025-04-17 11:22:43 +0700

    feat: add localization

--- src/main/kotlin/aelsi2/logisim_time/TimeStrings.kt
+++ src/main/kotlin/aelsi2/logisim_time/TimeStrings.kt
@@ -1,19 +1,35 @@
 package aelsi2.logisim_time
 
+import com.cburch.logisim.util.LocaleListener
 import com.cburch.logisim.util.LocaleManager
 import com.cburch.logisim.util.StringGetter
+import java.util.Locale
+import java.util.ResourceBundle
 
+@Suppress("UNCHECKED_CAST")
 object TimeStrings {
-    const val LIBRARY_NAME = "Time"
-    const val COMPONENT_STABLE_CLOCK = "Stable Clock"
-    const val PORT_LABEL_ENABLE = "en"
-    const val PORT_TOOLTIP_ENABLE = "Enable: When 0, output is set to 0"
-    const val PORT_TOOLTIP_CLOCK = "Clock: Updates on rising edge"
-    const val PORT_TOOLTIP_OUTPUT = "Current clock output"
-    const val ATTRIBUTE_DELAY = "Delay (ms)"
+    const val LIBRARY_NAME = "library_name"
+    const val COMPONENT_STABLE_CLOCK = "component_stable_clock"
+    const val PORT_LABEL_ENABLE = "port_label_enable"
+    const val PORT_TOOLTIP_ENABLE = "port_tooltip_enable"
+    const val PORT_TOOLTIP_CLOCK = "port_tooltip_clock"
+    const val PORT_TOOLTIP_OUTPUT = "port_tooltip_output"
+    const val ATTRIBUTE_DELAY = "attribute_delay"
 
-    private val source: LocaleManager = LocaleManager("resources/logisim", "std")
+    init {
+        // Pizdec
+        val field = LocaleManager::class.java.getDeclaredField("listeners")
+        field.isAccessible = true
+        val listeners = field.get(null) as ArrayList<LocaleListener>
+        listeners.add(0) {
+            bundle = loadBundle(LocaleManager.getLocale())
+        }
+    }
 
-    fun getter(key: String): StringGetter = source.getter(key)
-    fun get(key: String): String = source.get(key)
+    private var bundle = loadBundle(LocaleManager.getLocale())
+
+    fun getter(key: String): StringGetter = StringGetter { bundle.getString(key) }
+    fun get(key: String): String = bundle.getString(key)
+
+    private fun loadBundle(locale: Locale): ResourceBundle = ResourceBundle.getBundle("logisim_time", locale)
 }
